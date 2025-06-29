c4f6a03f6b7e42ea9d9b3180ba78acc192fd66b7
Alireza <alireza.rohollahi031@gmail.com>
2025-06-29 10:36:40 +0330

    add jobs

---
+++ jobs.sql
@@ -0,0 +1,34 @@
+use msdb;
+go 
+
+exec sp_add_job 'calculate_fine';
+
+go
+exec sp_add_jobstep 
+@job_name = 'calculate_fine' , 
+@step_name = 'execute procedure',
+@subsystem = 'TSQL',
+@database_name = 'EduLibDb',
+@command = 'exec poc_calculate_fine;',
+@on_success_action  = 1;
+
+go
+exec sp_add_schedule 
+@schedule_name ='daily fine' ,
+@freq_type  = 4,
+@freq_interval = 1,
+@active_start_time = 080000;
+
+go
+exec sp_attach_schedule 
+@job_name = 'calculate_fine',
+@schedule_name ='daily fine';
+
+go 
+exec sp_add_jobserver 
+@job_name = 'calculate_fine';
+
+go
+
+
+
